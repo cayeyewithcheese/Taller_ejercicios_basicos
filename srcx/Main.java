08b645f3d1cd14979f005a1fb2edd8490c7b4c7b
mel <meroqui31@gmail.com>
2026-09-18 11:38:43 -0500

    point 1

--- srcx/Main.java
+++ srcx/Main.java
@@ -1,9 +1,14 @@
-import java.util.Scanner;
 public class Main{
+    public static double calcularPromedio( double n1, double n2, double n3, double n4, double n5){
+        double notaFinal = (n1 * 0.15) + (n2 * 0.20) + (n3 * 0.15) + (n4 * 0.30) + (n5 * 0.20);
+        return notaFinal;
+    }
+
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
+/*
         System.out.println("Ingrese un numero decimal:");
         double numero = sc.nextDouble();
         int parteEntera = (int) numero;
@@ -11,7 +16,8 @@ public class Main{
         System.out.println("parte entera:" + parteEntera);
         System.out.println("parte decimal:" + parteDecimal);
         sc.close();
-
+*/
+        misAlgoritmos.punto1();
 
     }
 }
---
+++ srcx/MisAlgoritmos.java
@@ -0,0 +1,11 @@
+public class MisAlgoritmos {
+    void punto1(){
+        String texto1 = "Hola ";
+        String texto2 = "Mundo";
+        String Resultado = texto1 + "" + texto2;
+        System.out.println(Resultado);
+    }
+    void punto2(){
+
+    }
+}
