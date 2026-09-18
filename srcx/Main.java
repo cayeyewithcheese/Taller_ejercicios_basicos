9fbf5fcef84e95615de2556b391f765629f3f90f
mel <meroqui31@gmail.com>
2026-09-18 11:46:22 -0500

    guardando punto 2

--- srcx/Main.java
+++ srcx/Main.java
@@ -17,8 +17,8 @@ public class Main{
         System.out.println("parte decimal:" + parteDecimal);
         sc.close();
 */
-        misAlgoritmos.punto1();
 
+        misAlgoritmos.punto2();
     }
 }
 
--- srcx/MisAlgoritmos.java
+++ srcx/MisAlgoritmos.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class MisAlgoritmos {
     void punto1(){
         String texto1 = "Hola ";
@@ -6,6 +7,18 @@ public class MisAlgoritmos {
         System.out.println(Resultado);
     }
     void punto2(){
+            int num1 = 9;
+            int num2 = 2;
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int multiplicacion = num1 * num2;
+            int division = num1 / num2;
+            int residuo = num1 % num2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Porfavor ingrese su nombre");
+        String nombre = sc.nextLine();
+
+        System.out.println("Hola" + " " + nombre);
 
     }
 }
