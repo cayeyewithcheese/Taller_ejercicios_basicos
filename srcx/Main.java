9ab9d5a6510e57f7b1950ba2faeb99cb513a8a15
mel <meroqui31@gmail.com>
2026-09-18 11:01:29 -0500

    ejercicio 4 suma

--- srcx/Main.java
+++ srcx/Main.java
@@ -3,10 +3,11 @@ public class Main{
 
 
     public static void main(String[] args) {
-        int numero = 4;
-        int cuadrado = numero * numero;
+        int numero1 = 4;
+        int numero2 = 5;
+        int suma = numero1 + numero2;
 
-        System.out.println("El cuadrado de :" + numero +" es : " + cuadrado);
+        System.out.println("La suma es: " + suma);
     }
 }
 
