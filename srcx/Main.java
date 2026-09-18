21ed7ef600cf4c5ffb6b25eda670fc799dea58db
mel <meroqui31@gmail.com>
2026-09-18 10:26:03 -0500

    Parte 1
    ejercicio 1

---
+++ srcx/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        String texto1 = "Hola ";
+        String texto2 = "Mundo";
+        String Resultado = texto1 + "" + texto2;
+        System.out.println(Resultado);
+    }
+}
